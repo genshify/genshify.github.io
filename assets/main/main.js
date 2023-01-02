76b9954b4ee7551d862b2a15ac5ba250d44d38e4
iambasilp <basilxg321@gmail.com>
2023-01-02 15:49:16 +0530

    footer and scroll with scroll bar added

--- assets/main/main.js
+++ assets/main/main.js
@@ -2,13 +2,13 @@ const navToggle = document.getElementById('nav-toggle')
 const navClose = document.getElementById('nav-close')
 const navMenu = document.getElementById('nav-menu')
 
-if(navToggle){
-    navToggle.addEventListener('click',()=>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
-if(navClose){
-    navClose.addEventListener('click',()=>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -17,8 +17,39 @@ if(navClose){
 
 const navLinks = document.querySelectorAll('.nav__link')
 console.log(navLinks)
-navLinks.forEach((navLink)=>{
-    navLink.addEventListener('click',()=>{
+navLinks.forEach((navLink) => {
+    navLink.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 })
+
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 100) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
