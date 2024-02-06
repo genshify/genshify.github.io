ab6b1f0bc1fe20bd77a0bc16d084add7c682759d
ANAS <anaspappadan@gmail.com>
2024-02-06 13:20:23 +0530

    Add nahida image to About and Home pages

--- client/src/pages/About/About.tsx
+++ client/src/pages/About/About.tsx
@@ -1,5 +1,24 @@
+import nahida from "../../assets/images/nahida-max.webp"
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about__container grid">
+      <div className="about__data">
+        <h2 className="section__title-center">
+          Find Out A Little More <br />
+          About Us
+        </h2>
+        <p className="about__description">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae
+          neque quam enim accusantium autem ipsam incidunt dolores delectus iure
+          adipisci?
+        </p>
+      </div>
+
+      <img
+        src={nahida}
+        alt="about image"
+        className="about__img"
+      />
+    </div>
+  );
 }
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,26 +1,32 @@
+import "./home.css";
+import nahida from "../../assets/images/nahida-max.webp"
 export default function home() {
   return (
-    <div>
-        <section className="home" id="home">
-      <div className="home__container container grid">
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/5e5896a5-4a79-496a-bea4-81f26cfa2650/dfk0ut3-fb4e6ebd-5c41-41e5-98c1-35cdb0ab8d53.png/v1/fill/w_786,h_1016/nahida_genshin_impact_character_render_by_deg5270_dfk0ut3-pre.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTY1NCIsInBhdGgiOiJcL2ZcLzVlNTg5NmE1LTRhNzktNDk2YS1iZWE0LTgxZjI2Y2ZhMjY1MFwvZGZrMHV0My1mYjRlNmViZC01YzQxLTQxZTUtOThjMS0zNWNkYjBhYjhkNTMucG5nIiwid2lkdGgiOiI8PTEyODAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.AtqnmnR4uN1v657CtgthGOEuuQ0fVP91ZD4OYTyMKUw" alt="image" className="home__img" />
+    <div className="home">
+        <div className="home__container container grid">
+          <img
+            src={nahida}
+            alt="image"
+            className="home__img"
+          />
 
-        <div className="home__data">
-          <h1 className="home__title">
-            The <br />
-            Land of Wisdom
-          </h1>
-          <p className="home__description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
-            itaque, tempora fugit adipisci quo animi.
-          </p>
+          <div className="home__data">
+            <h1 className="home__title">
+              The <br />
+              Land of Wisdom
+            </h1>
+            <p className="home__description">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
+              itaque, tempora fugit adipisci quo animi.
+            </p>
 
-          <a href="#" className="button button-flex">Take a Tour
-            <i className="ri-arrow-right-line button-icon"></i>
-          </a>
+            <a href="#" className="button button-flex">
+              Take a Tour
+              <i className="ri-arrow-right-line button-icon"></i>
+            </a>
+          </div>
+
         </div>
-      </div>
-    </section>
     </div>
-  )
+  );
 }
