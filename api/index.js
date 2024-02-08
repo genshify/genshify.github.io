20770bf64b101e424df4b6463e2b8027cf0bc170
ANAS <anaspappadan@gmail.com>
2024-02-08 10:47:53 +0530

    add enka api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,36 @@
-import express from 'express';
+import express from "express";
+import { Wrapper } from "enkanetwork.js";
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
+
+//enka-network-api
+
+app.use(express.json());
+
+const { genshin} = new Wrapper({
+  cache: true
+});
+genshin
+  .getPlayer(738081787)
+  .then((player) => console.log(player.characters))
+  .catch((err) => console.log(err));
+
+// const showDetails = () => {
+// try {
+//   const enka = new EnkaClient();
+//   enka.fetchUser(825436941).then((user) => {
+//     console.log(hi);;
+//   });
+// } catch (error) {
+//     console.log(error);
+// }
+// }
+
+// app.get("/",(req,res) => {
+
+//   res.send(showDetails());
+// });
