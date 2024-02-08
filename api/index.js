a30fd63de2392b40609515d99598ff67a8d746d9
ANAS <anaspappadan@gmail.com>
2024-02-08 12:00:26 +0530

    complete the serach player api functionality

--- api/index.js
+++ api/index.js
@@ -6,31 +6,17 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
-
-//enka-network-api
-
 app.use(express.json());
 
+
+//  enka netowork api
 const { genshin} = new Wrapper({
   cache: true
 });
-genshin
-  .getPlayer(738081787)
-  .then((player) => console.log(player.characters))
-  .catch((err) => console.log(err));
 
-// const showDetails = () => {
-// try {
-//   const enka = new EnkaClient();
-//   enka.fetchUser(825436941).then((user) => {
-//     console.log(hi);;
-//   });
-// } catch (error) {
-//     console.log(error);
-// }
-// }
-
-// app.get("/",(req,res) => {
-
-//   res.send(showDetails());
-// });
+app.get("/genshin/player/:id",(req,res) => {
+  genshin
+  .getPlayer(req.params.id)
+  .then((player) => res.json(player))
+  .catch((err) => res.json(err));
+})
--- client/src/pages/Showcase/Showcase.tsx
+++ client/src/pages/Showcase/Showcase.tsx
@@ -1,28 +1,37 @@
 import "./showcase.css";
 
+
+
 export default function Showcase() {
-  const searchChar = async () => {
+
+  // ? gets the character details from enka api
+  const searchPlayer = async () => {
     const uid = (document.getElementById("uidInput") as HTMLInputElement).value;
     console.log(uid);
+    const res = await fetch(`/api/genshin/player/${uid}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    console.log(data);
   };
 
-
   return (
-    <div>
-      <section className="char container section" id="char">
-        <h2 className="section__title-center">Character Details</h2>
-        <div className="char__container grid">
-          <div className="input__box">
-            {/*test Uid: 825436941 */}
-            <input id="uidInput" type="text" placeholder="Enter your id" />
-            <button onClick={searchChar} className="char__search">
-              <p>Search</p>
-            </button>
-          </div>
-          <p className="cardDarkColor">It will shows your character</p>
-          <div id="chars--container"></div>
+    <div className="char container section" id="char">
+      <h2 className="section__title-center">Character Details</h2>
+      <div className="char__container grid">
+        <div className="input__box">
+          {/*test Uid: 825436941 */}
+          <input id="uidInput" type="text" placeholder="Enter your id" />
+          <button onClick={searchPlayer} className="char__search">
+            <p>Search</p>
+          </button>
         </div>
-      </section>
+        <p className="cardDarkColor">It will shows your character</p>
+        <div id="chars--container"></div>
+      </div>
     </div>
   );
 }
