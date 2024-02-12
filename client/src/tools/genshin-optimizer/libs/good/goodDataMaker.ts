218a4b7ad8614239614e1f1a7a1f9a7f0481ab8c
ANAS <anaspappadan@gmail.com>
2024-02-12 22:10:10 +0530

    feat: create charactersData object to use in "good"

--- client/src/tools/genshin-optimizer/libs/good/goodDataMaker.ts
+++ client/src/tools/genshin-optimizer/libs/good/goodDataMaker.ts
@@ -1,25 +1,37 @@
-import { IGOOD } from "."; // Import your interfaces
+import { ICharacterTalent, IGOOD } from "."; // Import your interfaces
+import { AscensionKey, CharacterKey } from "../consts";
 
 export const generateJSON = (data) => {
+  //to remove spaces from character name
+  const removeSpaces = (input: string) => {
+    return input.replace(/\s/g, "");
+  };
+
+  const charactersData =
+  // ? maps through the characters and store their properties
+    data.characters.map((char) => ({
+      key: removeSpaces(char.name) as CharacterKey,
+      level: parseInt(char.properties.level.val),
+      ascension: parseInt(char.properties.ascension.val) as AscensionKey,
+      constellation: char.constellationsList.length,
+      talent: {
+        auto: char.skills.normalAttacks.level,
+        skill: char.skills.elementalSkill.level,
+        burst: char.skills.elementalBurst.level,
+      } as ICharacterTalent,
+    }))
+
   // Create your data object of type IGOOD
-  const playerData: IGOOD = {
+  const goodData: IGOOD = {
     format: "GOOD",
     source: "Genshify",
     version: 1,
-    characters: [
-      {
-        key: "KaedeharaKazuha",
-        level: 1,
-        ascension: 0,
-        constellation: 0,
-        talent: { auto: 1, skill: 1, burst: 1 },
-      },
-    ],
+    characters: charactersData,
     artifacts: [],
     weapons: [],
   };
 
   // Convert data object to JSON string
-  const jsonData = JSON.stringify(playerData, null, 2);
+  const jsonData = JSON.stringify(goodData, null, 2);
   console.log(jsonData);
 };
