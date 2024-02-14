2ccd0f515dfb984c8e67f8cfdb231deb3073b9df
ANAS <anaspappadan@gmail.com>
2024-02-14 21:16:35 +0530

    spelling corrections

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import "./Navbar.css";
+import "./navbar.css";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/logo/Genshify_logo_for_Light.png";
 export default function navbar() {
