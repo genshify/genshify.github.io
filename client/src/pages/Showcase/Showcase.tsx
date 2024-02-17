457638c926e6486db0e626b6506253052e31ddd4
ANAS <anaspappadan@gmail.com>
2024-02-17 18:57:58 +0530

    added character side icons in showcase

--- client/src/pages/Showcase/Showcase.tsx
+++ client/src/pages/Showcase/Showcase.tsx
@@ -25,11 +25,9 @@ export default function Showcase() {
   };
 
   const showCharacterDetails = (id: number) => {
-    // ? scrolls to the character details section
+    // ? shows the selected (clicked) character
     setCharIndex(id);
     setShowChar(true);
-    const element = document.getElementById("charDetails");
-    if (element) element.scrollIntoView({ behavior: "smooth" });
   };
 
   const [jsonData, setJsonData] = useState<string>("");
@@ -54,6 +52,179 @@ export default function Showcase() {
       {test2 && (
         <div>
           <h1>{test2.player.username}'s Characters</h1>
+          <div className="charSideIconContainer">
+            {test2.characters.map((character, index) => (
+              <div
+                key={index}
+                onClick={() => showCharacterDetails(index)}
+                className={`charSideIcon bg_${character.element}`}
+              >
+                <img
+                  src={`https://enka.network/ui/${character.assets.sideIcon}.png`}
+                />
+              </div>
+            ))}
+          </div>
+          {/* selected character details section */}
+          <section id="charDetails">
+            {showChar && test2 && (
+              <div
+                className={`char__details bg_${test2.characters[charIndex].element}`}
+              >
+                <div className="charDivRow1">
+                  <div className="charCardCol1 charIconName cardDarkColor">
+                    <img
+                      className="charIcon"
+                      src={`https://enka.network/ui/${test2.characters[charIndex].assets.icon}.png`}
+                    />
+                    <p className="charName">
+                      {test2.characters[charIndex].name}
+                    </p>
+                    <p className="charLvl">
+                      {test2.characters[charIndex].maxLevel}
+                    </p>
+                  </div>
+                  <div className="charStatsInfo charCardCol2 cardDarkColor">
+                    <div className="charStatsInfoCol1">
+                      <p>
+                        hp:
+                        {Math.round(
+                          Number(test2.characters[charIndex].stats.maxHp.value)
+                        )}
+                      </p>
+                      <p>
+                        Atk :{" "}
+                        {Math.round(
+                          Number(test2.characters[charIndex].stats.atk.value)
+                        )}
+                      </p>
+                      <p>
+                        Def :{" "}
+                        {Math.round(
+                          Number(test2.characters[charIndex].stats.def.value)
+                        )}
+                      </p>
+                      <p>
+                        Em :{" "}
+                        {Math.round(
+                          Number(
+                            test2.characters[charIndex].stats.elementalMastery
+                              .value
+                          )
+                        )}
+                      </p>
+                    </div>
+                    <div className="charStatsInfoCol2">
+                      <p>
+                        Cr :{" "}
+                        {Math.round(
+                          Number(
+                            test2.characters[charIndex].stats.critRate.value
+                          ) * 100
+                        )}
+                        %
+                      </p>
+                      <p>
+                        Cd :{" "}
+                        {Math.round(
+                          Number(
+                            test2.characters[charIndex].stats.critDamage.value
+                          ) * 100
+                        )}
+                        %
+                      </p>
+                      <p>
+                        Er :{" "}
+                        {Math.round(
+                          Number(
+                            test2.characters[charIndex].stats.energyRecharge
+                              .value
+                          ) * 100
+                        )}
+                        %
+                      </p>
+                      <p>
+                        {test2.characters[charIndex].element}dmg% :{" "}
+                        {Math.round(
+                          Number(
+                            test2.characters[charIndex].stats.pyroDamageBonus
+                              .value
+                          ) * 100
+                        )}
+                        %
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="charDivRow2">
+                  <div className="charWeapon charCardCol1 cardDarkColor">
+                    <p className="weaponP">
+                      <img
+                        className=" weaponImg"
+                        src={`https://enka.network/ui/${test2.characters[charIndex].equipment.weapon.assets.icon}.png`}
+                        alt=""
+                      />
+                      {test2.characters[charIndex].equipment.weapon.level}
+                    </p>
+                    <p className="weaponName">
+                      {test2.characters[charIndex].equipment.weapon.name}
+                    </p>
+                  </div>
+                  <div className="charCardCol2 cardDarkColor">
+                    <p className="talentP">
+                      <img
+                        className="talentImg"
+                        src={`https://enka.network/ui/${test2.characters[charIndex].assets.talents.normalAttack}.png`}
+                        alt=""
+                      />
+                      {test2.characters[charIndex].skills.normalAttacks.level}
+                    </p>
+                    <p className="talentP">
+                      <img
+                        className="talentImg"
+                        src={`https://enka.network/ui/${test2.characters[charIndex].assets.talents.elementalSkill}.png`}
+                        alt=""
+                      />
+                      {test2.characters[charIndex].skills.elementalSkill.level}
+                    </p>
+                    <p className="talentP">
+                      <img
+                        className="talentImg"
+                        src={`https://enka.network/ui/${test2.characters[charIndex].assets.talents.elementalBurst}.png`}
+                        alt=""
+                      />
+                      {test2.characters[charIndex].skills.elementalBurst.level}
+                    </p>
+                  </div>
+                </div>
+                <div className="charDivRow3">
+                  <div className="charConstl cardDarkColor">
+                    {test2.player.showcase[charIndex].assets.constellations.map(
+                      (constellation, index) => (
+                        <div key={index} className="constlCircle">
+                          {test2.characters[charIndex].constellationsList[
+                            index
+                          ] ? (
+                            <img
+                              className="constlImg"
+                              src={`https://enka.network/ui/${constellation}.png`}
+                              alt=""
+                            />
+                          ) : (
+                            <img
+                              className="constlImgDisabled"
+                              src={`https://enka.network/ui/${constellation}.png`}
+                              alt=""
+                            />
+                          )}
+                        </div>
+                      )
+                    )}
+                  </div>
+                </div>
+              </div>
+            )}
+          </section>
           <div className="char_cards_container">
             {test2.characters.map((character, index) => (
               <div
@@ -167,158 +338,6 @@ export default function Showcase() {
         </div>
       )}
 
-      {/* selected character details section */}
-      <section id="charDetails">
-        {showChar && test2 && (
-          <div
-            className={`char__details bg_${test2.characters[charIndex].element}`}
-          >
-            <div className="charDivRow1">
-              <div className="charCardCol1 charIconName cardDarkColor">
-                <img
-                  className="charIcon"
-                  src={`https://enka.network/ui/${test2.characters[charIndex].assets.icon}.png`}
-                />
-                <p className="charName">{test2.characters[charIndex].name}</p>
-                <p className="charLvl">
-                  {test2.characters[charIndex].maxLevel}
-                </p>
-              </div>
-              <div className="charStatsInfo charCardCol2 cardDarkColor">
-                <div className="charStatsInfoCol1">
-                  <p>
-                    hp:
-                    {Math.round(
-                      Number(test2.characters[charIndex].stats.maxHp.value)
-                    )}
-                  </p>
-                  <p>
-                    Atk :{" "}
-                    {Math.round(
-                      Number(test2.characters[charIndex].stats.atk.value)
-                    )}
-                  </p>
-                  <p>
-                    Def :{" "}
-                    {Math.round(
-                      Number(test2.characters[charIndex].stats.def.value)
-                    )}
-                  </p>
-                  <p>
-                    Em :{" "}
-                    {Math.round(
-                      Number(
-                        test2.characters[charIndex].stats.elementalMastery.value
-                      )
-                    )}
-                  </p>
-                </div>
-                <div className="charStatsInfoCol2">
-                  <p>
-                    Cr :{" "}
-                    {Math.round(
-                      Number(test2.characters[charIndex].stats.critRate.value) *
-                        100
-                    )}
-                    %
-                  </p>
-                  <p>
-                    Cd :{" "}
-                    {Math.round(
-                      Number(
-                        test2.characters[charIndex].stats.critDamage.value
-                      ) * 100
-                    )}
-                    %
-                  </p>
-                  <p>
-                    Er :{" "}
-                    {Math.round(
-                      Number(
-                        test2.characters[charIndex].stats.energyRecharge.value
-                      ) * 100
-                    )}
-                    %
-                  </p>
-                  <p>
-                    {test2.characters[charIndex].element}dmg% :{" "}
-                    {Math.round(
-                      Number(
-                        test2.characters[charIndex].stats.pyroDamageBonus.value
-                      ) * 100
-                    )}
-                    %
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="charDivRow2">
-              <div className="charWeapon charCardCol1 cardDarkColor">
-                <p className="weaponP">
-                  <img
-                    className=" weaponImg"
-                    src={`https://enka.network/ui/${test2.characters[charIndex].equipment.weapon.assets.icon}.png`}
-                    alt=""
-                  />
-                  {test2.characters[charIndex].equipment.weapon.level}
-                </p>
-                <p className="weaponName">
-                  {test2.characters[charIndex].equipment.weapon.name}
-                </p>
-              </div>
-              <div className="charCardCol2 cardDarkColor">
-                <p className="talentP">
-                  <img
-                    className="talentImg"
-                    src={`https://enka.network/ui/${test2.characters[charIndex].assets.talents.normalAttack}.png`}
-                    alt=""
-                  />
-                  {test2.characters[charIndex].skills.normalAttacks.level}
-                </p>
-                <p className="talentP">
-                  <img
-                    className="talentImg"
-                    src={`https://enka.network/ui/${test2.characters[charIndex].assets.talents.elementalSkill}.png`}
-                    alt=""
-                  />
-                  {test2.characters[charIndex].skills.elementalSkill.level}
-                </p>
-                <p className="talentP">
-                  <img
-                    className="talentImg"
-                    src={`https://enka.network/ui/${test2.characters[charIndex].assets.talents.elementalBurst}.png`}
-                    alt=""
-                  />
-                  {test2.characters[charIndex].skills.elementalBurst.level}
-                </p>
-              </div>
-            </div>
-            <div className="charDivRow3">
-              <div className="charConstl cardDarkColor">
-                {test2.player.showcase[charIndex].assets.constellations.map(
-                  (constellation, index) => (
-                    <div key={index} className="conslCircle">
-                      {test2.characters[charIndex].constellationsList[index] ? (
-                        <img
-                          className="constlImg"
-                          src={`https://enka.network/ui/${constellation}.png`}
-                          alt=""
-                        />
-                      ) : (
-                        <img
-                          className="constlImgDisabled"
-                          src={`https://enka.network/ui/${constellation}.png`}
-                          alt=""
-                        />
-                      )}
-                    </div>
-                  )
-                )}
-              </div>
-            </div>
-          </div>
-        )}
-      </section>
       {playerDetails && (
         <div>
           <button onClick={() => setJsonData(generateJSON(playerDetails))}>
@@ -331,14 +350,13 @@ export default function Showcase() {
                 disabled
                 rows={20}
                 value={jsonData}
-                className="jsonfield"
+                className="jsonField"
               ></textarea>
               <button
                 id="copy"
                 onClick={() => {
                   // ? copies the json data to the clipboard
                   navigator.clipboard.writeText(jsonData);
-                  // ? shows a message that the data has been copied by changine the button text to "copied to clipboard"
                   const copyBtn = document.getElementById("copy");
                   if (copyBtn) copyBtn.textContent = "Copied to Clipboard";
                 }}
