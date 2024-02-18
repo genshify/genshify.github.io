6dc3904de275c992eec82ade12cf69e34d03137c
ANAS <anaspappadan@gmail.com>
2024-02-18 13:26:05 +0530

    Fix import paths and update dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import { Route, BrowserRouter, Routes } from "react-router-dom";
-
 //import components
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
@@ -9,23 +7,88 @@ import Showcase from "./pages/Showcase/Showcase";
 import CharacterDisplay from "./tools/genshin-optimizer/app/PageCharacter/CharacterDisplay";
 import PageCharacter from "./tools/genshin-optimizer/app/PageCharacter";
 
+import {
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+} from "@mui/material";
+
+import { DatabaseContext } from "genshin-optimizer/db-ui";
+import { theme } from "genshin-optimizer/ui";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+
+import ErrorBoundary from "./tools/genshin-optimizer/app/ErrorBoundary";
+import "./App.scss"
+
+import { Suspense, useCallback, useMemo, useState } from "react";
+import { ArtCharDatabase } from "genshin-optimizer/db";
+import { DBLocalStorage, SandboxStorage } from "genshin-optimizer/database";
+
 export default function App() {
+  const dbIndex = parseInt(localStorage.getItem("dbIndex") || "1");
+  const [databases, setDatabases] = useState(() => {
+    localStorage.removeItem("GONewTabDetection");
+    localStorage.setItem("GONewTabDetection", "debug");
+    return ([1, 2, 3, 4] as const).map((index) => {
+      if (index === dbIndex) {
+        return new ArtCharDatabase(index, new DBLocalStorage(localStorage));
+      } else {
+        const dbName = `extraDatabase_${index}`;
+        const eDB = localStorage.getItem(dbName);
+        const dbObj = eDB ? JSON.parse(eDB) : {};
+        const db = new ArtCharDatabase(index, new SandboxStorage(dbObj));
+        db.toExtraLocalDB();
+        return db;
+      }
+    });
+  });
+  const setDatabase = useCallback(
+    (index: number, db: ArtCharDatabase) => {
+      const dbs = [...databases];
+      dbs[index] = db;
+      setDatabases(dbs);
+    },
+    [databases, setDatabases]
+  );
+
+  const database = databases[dbIndex - 1];
+  const dbContextObj = useMemo(
+    () => ({ databases, setDatabases, database, setDatabase }),
+    [databases, setDatabases, database, setDatabase]
+  );
   return (
-    <BrowserRouter>
-      {/* navbar */}
-      <div>
-        <Navbar />
-      </div>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/beginner" element={<Beginner />} />
-        <Route path="/showcase" element={<Showcase />} />
-        <Route path="/characters/*">
-              <Route index element={<PageCharacter />} />
-              <Route path=":characterKey/*" element={<CharacterDisplay />} />
-            </Route>
-      </Routes>
-    </BrowserRouter>
+    <StyledEngineProvider injectFirst>
+      {/* https://mui.com/guides/interoperability/#css-injection-order-2 */}
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <DatabaseContext.Provider value={dbContextObj}>
+          <ErrorBoundary>
+            <HashRouter basename="/">
+              <Suspense fallback={null}></Suspense>
+          
+                {/* navbar */}
+                <div>
+                  <Navbar/>
+                </div>
+                
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/beginner" element={<Beginner />} />
+                  <Route path="/showcase" element={<Showcase />} />
+                  <Route path="/characters/*">
+                    <Route index element={<PageCharacter />} />
+                    <Route
+                      path=":characterKey/*"
+                      element={<CharacterDisplay />}
+                    />
+                  </Route>
+                </Routes>
+              
+            </HashRouter>
+          </ErrorBoundary>
+        </DatabaseContext.Provider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
